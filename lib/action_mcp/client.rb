bc84bae9f76b7dabd5b8819a36269ad8e24a4056
Abdelkader Boudih <terminale@gmail.com>
2025-05-12 21:43:17 +0100

    feat: remove stdio transport (#56)

--- lib/action_mcp/client.rb
+++ lib/action_mcp/client.rb
@@ -3,27 +3,21 @@
 module ActionMCP
   # Creates a client appropriate for the given endpoint.
   #
-  # @param endpoint [String] The endpoint to connect to (URL or command).
+  # @param endpoint [String] The endpoint to connect to (URL).
   # @param logger [Logger] The logger to use. Default is Logger.new($stdout).
   # @param options [Hash] Additional options to pass to the client constructor.
   #
-  # @return [Client::SSEClient, Client::StdioClient] An instance of either SSEClient or StdioClient
-  #   depending on the format of the endpoint.
+  # @return [Client::SSEClient] An instance of SSEClient for HTTP(S) endpoints.
   #
   # @example
   #   client = ActionMCP.create_client("http://127.0.0.1:3001/action_mcp")
   #   client.connect
-  #
-  # @example
-  #   client = ActionMCP.create_client("some_command")
-  #   client.execute
   def self.create_client(endpoint, logger: Logger.new($stdout), **options)
     if endpoint =~ %r{\Ahttps?://}
       logger.info("Creating SSE client for endpoint: #{endpoint}")
       Client::SSEClient.new(endpoint, logger: logger, **options)
     else
-      logger.info("Creating STDIO client for command: #{endpoint}")
-      Client::StdioClient.new(endpoint, logger: logger, **options)
+      raise ArgumentError, "Only HTTP(S) endpoints are supported. STDIO and other transports are not supported."
     end
   end
 
