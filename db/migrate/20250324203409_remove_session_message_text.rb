d83bba8ff4ac730a0ebf26e1a13cb35e732f8cae
Abdelkader Boudih <terminale@gmail.com>
2025-05-12 20:32:31 +0100

    chore: cleanup migrations

--- db/migrate/20250324203409_remove_session_message_text.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveSessionMessageText < ActiveRecord::Migration[8.0]
-  def up
-    remove_column :action_mcp_session_messages, :message_text
-  end
-end
--- db/migrate/20250327124131_add_sse_event_counter_to_action_mcp_sessions.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddSSEEventCounterToActionMCPSessions < ActiveRecord::Migration[8.0]
-  def change
-    add_column :action_mcp_sessions, :sse_event_counter, :integer, default: 0, null: false
-  end
-end
