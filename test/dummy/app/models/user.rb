7679b5bacaf719ee873b0c8a11d8efa3ba5f0d38
Abdelkader Boudih <terminale@gmail.com>
2025-07-04 15:51:53 +0100

    fix: implement protocol header detection

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -2,7 +2,7 @@
 #
 # Table name: users
 #
-#  id         :bigint           not null, primary key
+#  id         :integer          not null, primary key
 #  email      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
